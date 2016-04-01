7a1a4a6e654520a361258a8353dd2e194042253c
fmunozse <fmunozse@gmail.com>
2016-04-01 23:08:15 +0200

    #4 HomePage - Improve look&feel (3 columns)

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,9 +1,32 @@
 'use strict';
 
 angular.module('jtrackseriesApp')
-    .controller('MainController', function ($scope, Principal) {
+    .controller('MainController', function ($scope, $modal, Principal) {
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
+        
+        
+        $scope.openModalImage = function (imageSrc, imageDescription) {
+        	$modal.open({
+        		templateUrl: "scripts/app/main/modalImage.html",
+        		resolve: {
+        			imageSrcToUse: function () {
+        				return imageSrc;
+        			},
+        			imageDescriptionToUse: function () {
+        				return imageDescription;
+        			}
+        		},
+        		controller: [
+        		  "$scope", "imageSrcToUse", "imageDescriptionToUse",
+        			function ($scope, imageSrcToUse, imageDescriptionToUse) {
+        				$scope.ImageSrc = imageSrcToUse;
+        				return $scope.ImageDescription = imageDescriptionToUse;
+        		  }
+        		]
+        	});
+        };
+        
     });
