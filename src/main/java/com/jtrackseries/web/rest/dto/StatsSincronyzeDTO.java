4b53404211fac07aa93f1abb7e33bc1807bf4a81
fmunozse <fmunozse@gmail.com>
2016-04-18 22:55:50 +0200

    commit fix rebase

--- src/main/java/com/jtrackseries/web/rest/dto/StatsSincronyzeDTO.java
+++ src/main/java/com/jtrackseries/web/rest/dto/StatsSincronyzeDTO.java
@@ -5,7 +5,6 @@ public class StatsSincronyzeDTO {
 	public int seriesUpdated = 0;
 	public int episodesUpdated = 0;
 	public int episodesNewed = 0;
-<<<<<<< HEAD
 	public int episodesRemoved = 0;
 	
 	public String toString() {
@@ -16,8 +15,6 @@ public class StatsSincronyzeDTO {
                 ", episodes removed='" + episodesRemoved + "'" +
                 '}';
 	}
-=======
->>>>>>> 274f1678b1ceff64c0d07526cab34322f2cd5ca3
 	
 	public int getSeriesUpdated() {
 		return seriesUpdated;
@@ -37,15 +34,12 @@ public class StatsSincronyzeDTO {
 	public void setEpisodesNewed(int episodesNewed) {
 		this.episodesNewed = episodesNewed;
 	}
-<<<<<<< HEAD
 	public int getEpisodesRemoved() {
 		return episodesRemoved;
 	}
 	public void setEpisodesRemoved(int episodesRemoved) {
 		this.episodesRemoved = episodesRemoved;
 	}
-=======
->>>>>>> 274f1678b1ceff64c0d07526cab34322f2cd5ca3
 	
 	
 }
